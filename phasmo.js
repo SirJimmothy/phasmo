f079c853e00e4116169d7c677251e68cf65e7a7b
Sir_Jimmothy <85596958+SirJimmothy@users.noreply.github.com>
2022-01-30 14:26:59 +0000

    Updated Onryo useful info

--- phasmo.js
+++ phasmo.js
@@ -192,8 +192,8 @@ let ghosts = {
 			"50% chance to hunt when flame is extinguished, regardless of sanity (except first three blowouts)",
 			"+25% hunt chance per dead player",
 			"Can hunt from 60% sanity without flame in room",
-			"Flames act as crucifixes; 3M hunt blocking range. Has precedence over crucifix",
-			"When flame prevents hunt, it is blown out",
+			"Candles act as crucifixes; 3M hunt blocking range. Has precedence over crucifix",
+			"When candle prevents hunt, it is blown out",
 		],
 	},
 	"phantom": {
